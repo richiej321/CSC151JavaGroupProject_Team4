9755b9c0794006c35f210cfaf67ec66133073f55
Lelah Jones <77903182+lelahjones@users.noreply.github.com>
2022-06-29 20:00:40 -0600

    Update CarLot.java

--- CarLot.java
+++ CarLot.java
@@ -1,17 +1,21 @@
+import java.util.ArrayList;
+
 public class CarLot {
     // Via 11.11 The ArrayList Class in text book.
     ArrayList<Car> inventory = new ArrayList<>();
     // no need to use concrete type thanks to type interface
     
-public Car findCarByIdentifier(String identifier) {
-    // for every car in the inventory
-    for (Car car : inventory) {
-        // if -call getter car.getID()- equals the identifier (ID)
-        if (car.getId().equals(identifier)) {
-        // return the car id called
-	        return car;
-        }
+	 public Car findCarByIdentifier(String identifier) {
+		for (Car car : inventory) {
+		    if (car.getId().equals(identifier)) {  // accessor aka getter from part I
+			return car;
+		    }
+		}
+		return null;
+	    }
+	
+	public ArrayList<Car> getCarsInOrderOfEntry() {
+        return new ArrayList<>(this.inventory);
+	}
     }
-    // otherwise return null
-    return null;
-}
+
