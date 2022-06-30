359c69501044550c53a9b9c3af4a8bbdd4527aad
Lelah Jones <77903182+lelahjones@users.noreply.github.com>
2022-06-29 19:29:45 -0600

    Update CarLot.java

--- CarLot.java
+++ CarLot.java
@@ -1,2 +1,15 @@
 public class CarLot {
+    private ArrayList<Car> inventory = new ArrayList<>; 
+    
+public Car findCarByIdentifier(String identifier) {
+    // for every car in the inventory
+    for (Car car : inventory) {
+        // if -call getter car.getID()- equals the identifier (ID)
+        if (car.getId().equals(identifier)) {
+        // return the car id called
+	        return car;
+        }
+    }
+    // otherwise return null
+    return null;
 }
