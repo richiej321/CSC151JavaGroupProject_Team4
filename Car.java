7d8f935e8881cf6665fdb023bd250fb6030216e8
alimahm1 <108840597+alimahm1@users.noreply.github.com>
2022-07-11 16:11:03 -0400

    Update Car.java

--- Car.java
+++ Car.java
@@ -23,14 +23,128 @@ Accessors and Mutators for all attributes
 */
 
 public class Car {
-  // Constructor
-  
-  private String ID;
-  private int mileage;
-  private int mpg;
- 
-  private double cost;
-  private double askingPrice;
-  // boolean value
-  
+private String id;
+private int mileage;
+private int mpg;
+private double cost;
+private double askingPrice;
+private double salePrice;
+private boolean isSold;
+
+
+public Car() {}
+public Car(String id, int mileage, int mpg, double cost, double askingPrice) {
+this.id=id;
+this.mileage=mileage;
+this.mpg=mpg;
+this.cost=cost;
+this.askingPrice=askingPrice;
 }
+public String getId() {
+return this.id;
+}
+public int getMileage() {
+return this.mileage;
+}
+public int getMpg() {
+return this.mpg;
+}
+public double getCost() {
+return this.cost;
+}
+public double getAskingPrice() {
+return this.askingPrice;
+}
+public double getSalePrice() {
+return this.salePrice;
+}
+public boolean isSold() {
+return this.isSold;
+}
+
+public void setId(String id) {
+this.id=id;
+}
+public void setMileage(int mileage) {
+this.mileage=mileage;
+}
+public void setMpg(int mpg) {
+this.mpg=mpg;
+}
+public void setCost(double cost) {
+this.cost=cost;
+}
+public void setAskingPrice(double askingPrice) {
+this.askingPrice=askingPrice;
+}
+public void setSalePrice(double salePrice) {
+this.salePrice=salePrice;
+}
+public void setIsSold(boolean isSold) {
+this.isSold=isSold;
+}
+
+public String toString() {
+StringBuilder sb = new StringBuilder();
+sb.append("Car: "+this.id);
+sb.append(", Mileage: "+this.mileage);
+sb.append(", MPG: "+this.mpg);
+sb.append(", Sold: "+(this.isSold?"Yes":"No"));
+sb.append(", Cost: $"+this.cost);
+sb.append(", Selling Price: $"+this.askingPrice);
+
+if(this.isSold) {
+sb.append(", Sold For $"+this.salePrice);
+}
+
+return sb.toString();
+}
+
+int compareMPG(Car otherCar) throws Exception{
+if(otherCar!=null) {
+if(this.mpg<otherCar.getMpg()) {
+return -1;
+} else if(this.mpg==otherCar.getMpg()) {
+return 0;
+} else {
+return 1;
+}
+} else {
+throw new Exception("Cars can't be compared.");
+}
+}
+
+int compareMiles(Car otherCar) throws Exception{
+if(otherCar!=null) {
+if(this.mileage<otherCar.getMileage()) {
+return -1;
+} else if(this.mileage==otherCar.getMileage()) {
+return 0;
+} else {
+return 1;
+}
+} else {
+throw new Exception("Cars can't be compared.");
+}
+}
+
+int comparePrice(Car otherCar) throws Exception{
+if(otherCar!=null) {
+if(this.askingPrice<otherCar.getAskingPrice()) {
+return -1;
+} else if(this.askingPrice==otherCar.getAskingPrice()) {
+return 0;
+} else {
+return 1;
+}
+} else {
+throw new Exception("Cars can't be compared.");
+}
+}
+
+void sellCar(double salePrice) {
+this.isSold=true;
+this.salePrice=salePrice;
+}
+}
+
