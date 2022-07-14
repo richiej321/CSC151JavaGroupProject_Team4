5cc8c75f9a13e17760ca13128e44feb9c21aa3c7
Lelah Jones <77903182+lelahjones@users.noreply.github.com>
2022-07-13 20:45:07 -0400

    Update Car.java
    formatting, commented out duplicate code blocks at bottom properly

--- Car.java
+++ Car.java
@@ -21,70 +21,71 @@ Accessors and Mutators for all attributes
 
 public class Car {
 
-  private String id;
-  private int mileage;
-  private int mpg;
-  private double cost;
-  private double askingPrice;
-  private double salePrice;
-  private boolean isSold;
-}
+	private String id;
+	private int mileage;
+	private int mpg;
+	private double cost;
+	private double askingPrice;
+	private double salePrice;
+	private boolean isSold;
 
 
-public Car() {}
-  public Car(String id, int mileage, int mpg, double cost, double askingPrice) {
-	  this.id=id;
-	  this.mileage=mileage;
-	  this.mpg=mpg;
-	  this.cost=cost;
-	  this.askingPrice=askingPrice;
-}
-public String getId() {
-  return this.id;
-}
-public int getMileage() {
-  return this.mileage;
-}
-public int getMpg() {
-  return this.mpg;
-}
-public double getCost() {
-  return this.cost;
-}
-public double getAskingPrice() {
-  return this.askingPrice;
-}
-public double getSalePrice() {
-  return this.salePrice;
-}
-public boolean isSold() {
-  return this.isSold;
-}
+	public Car() {}
+	public Car(String id, int mileage, int mpg, double cost, double askingPrice) {
+		this.id=id;
+		this.mileage=mileage;
+		this.mpg=mpg;
+		this.cost=cost;
+		this.askingPrice=askingPrice;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+	public int getMileage() {
+		return this.mileage;
+	}
+	public int getMpg() {
+		return this.mpg;
+	}
+	public double getCost() {
+		return this.cost;
+	}
+	public double getAskingPrice() {
+		return this.askingPrice;
+	}
+	public double getSalePrice() {
+		return this.salePrice;
+	}
+	public boolean isSold() {
+		return this.isSold;
+	}
 
 
-public void setId(String id) {
-  this.id=id;
-}
-public void setMileage(int mileage) {
-  this.mileage=mileage;
-}
-public void setMpg(int mpg) {
-  this.mpg=mpg;
-}
-public void setCost(double cost) {
-  this.cost=cost;
-}
-public void setAskingPrice(double askingPrice) {
-  this.askingPrice=askingPrice;
-}
-public void setSalePrice(double salePrice) {
-  this.salePrice=salePrice;
-}
-public void setIsSold(boolean isSold) {
-  this.isSold=isSold;
+	public void setId(String id) {
+		this.id=id;
+	}
+	public void setMileage(int mileage) {
+		this.mileage=mileage;
+	}
+	public void setMpg(int mpg) {
+		this.mpg=mpg;
+	}
+	public void setCost(double cost) {
+		this.cost=cost;
+	}
+	public void setAskingPrice(double askingPrice) {
+		this.askingPrice=askingPrice;
+	}
+	public void setSalePrice(double salePrice) {
+		this.salePrice=salePrice;
+	}
+	public void setIsSold(boolean isSold) {
+		this.isSold=isSold;
+	}	
 }
 
-//
+/**
 
     private String id;
     private int mileage;
@@ -144,4 +145,4 @@ public void setIsSold(boolean isSold) {
     }
 }
     
-
+**/
