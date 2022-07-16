725717a2c9862609b3c255363c64b954a288a918
Lelah Jones <77903182+lelahjones@users.noreply.github.com>
2022-07-16 09:32:51 -0400

    Update CarLot.java

--- CarLot.java
+++ CarLot.java
@@ -1,33 +1,6 @@
-import java.util.ArrayList;
-//import java.util.ListIterator;
-
-/** Part II: CarLot class
-
-         * Define a new class, CarLot that meets the following specification:
-        * Attribute:
-        * •	inventory – represents all the cars in the CarLot. It should be of type ArrayList<Car>.
-        *
-        * Accessor and Mutator for the inventory attribute
-        *
-        * Additional methods:
-        * •	Car findCarByIdentifier(String identifier) -  Find the car with the specified identifer in the inventory.
-        *      	Return null if the Car is not found (input is a String, output is a Car type object or null)
-        * •	ArrayList<Car> getAllCars() - Return an ArrayList of all Cars in the inventory. This method should return a
-        * 		copy of the inventory, not the inventory itself. (No input, output is an ArrayList of Car objects)
-        * •	Car getCarWithBestMPG() - Return the Car in the inventory with the highest MPG. (No input, output is a Car type object or null if the inventory is empty.)
-        * •	Car getCarWithHighestMileage() - Return the Car in the inventory with the highest mileage. (No input, output is a Cartype object or null if
-        the inventory is empty.)
-        * •	double getAverageMpg() - Return the average MPG of all Cars in the inventory. (No input, output is a double value.)
-        * •	double getTotalProfit() - Return the total profit of all cars in the inventory that have been sold (No input, output is a double value.)
-        * •	void addCar(String id, int mileage, int mpg, double cost, double askingPrice) - Add a new Car with the specified id,
-        * 		mileage, mpg, cost, and askingPrice to the inventory (Input: a String, two int, two double values. No output.)
-        * •	void sellCar(String identifier, double salePrice ) throws IllegalArgumentException - Sell the Car identified by identifier for the salePrice.
-        *      If the Car does not exist by the given identifier, throw an IllegalArgument Exception with an appropriate error
-        *      message. (Input: a string and a double value. No output. Remember to mark the attribute isSold to be true if this method executes successfully.)
-        */
 
 import java.util.ArrayList;
-import java.util.ListIterator;
+
 
 /** Part II: CarLot class
 
@@ -55,10 +28,10 @@ import java.util.ListIterator;
         */
 
 
-public class CarLot {
+public class CarLot extends Car {
     //private ArrayList<Car> inventory;
     // Via 11.11 The ArrayList Class in text book.
-    private int mpg;
+
     private static ArrayList<Car> inventory = new ArrayList<>();
     // no need to use concrete type thanks to type interface
 
@@ -144,12 +117,12 @@ public class CarLot {
         return averageMpg;
     }
 
-    public static double getTotalProfit() {
-        double TotalProfit = 0;
-        for (Car car : inventory) {
-            TotalProfit = car.getCost() - car.getAskingPrice();
+    public double getTotalProfit() {
+        double profit = 0;
+        for(Car car : inventory) {
+            profit += car.getSalePrice();
         }
-        return TotalProfit;
+        return profit;
     }
 
 
@@ -164,6 +137,5 @@ public class CarLot {
         }
     }
 }
-
 
 
